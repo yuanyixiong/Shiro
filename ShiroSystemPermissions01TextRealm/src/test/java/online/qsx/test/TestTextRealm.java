e3efa5dad79e751def9d7b847f065a411130750e
yuanyixiong <15926499574@163.com>
2018-06-21 16:20:02 +0800

    Shiro Text Realm

---
+++ ShiroSystemPermissions01TextRealm/src/test/java/online/qsx/test/TestTextRealm.java
@@ -0,0 +1,35 @@
+package online.qsx.test;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+
+
+public class TestTextRealm {
+	public static void main(String[] args) {
+		// 读取配置文件，初始化SecurityManager工厂
+		Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro.ini");
+		// 获取securityManager实例
+		SecurityManager securityManager = factory.getInstance();
+		// 把securityManager实例绑定到SecurityUtils
+		SecurityUtils.setSecurityManager(securityManager);
+		// 创建token令牌，用户名/密码
+		UsernamePasswordToken token = new UsernamePasswordToken("arvin", "123");
+		// 得到当前执行的用户
+		Subject currentUser = SecurityUtils.getSubject();
+		try {
+			// 身份认证
+			currentUser.login(token);
+			System.out.println("身份认证成功！");
+		} catch (AuthenticationException e) {
+			e.printStackTrace();
+			System.out.println("身份认证失败！");
+		}
+		// 退出
+		currentUser.logout();
+	}
+}
